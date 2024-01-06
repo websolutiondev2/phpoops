b0a520fbfc3f231a95a1068c7815be81a7c555a4
websolutiondev2 <awebsolutionsp2@gmail.com>
2024-01-06 14:05:59 +0530

    changes

--- config.php
+++ config.php
@@ -2,5 +2,5 @@
     define("DB_HOST", 'localhost');  
     define("DB_USER", 'root');  
     define("DB_PASSWORD", '');  
-    define("DB_DATABSE", 'login_db');  
+    define("DB_DATABASE", 'login_db');  
 ?>  
--- dbConnect.php
+++ dbConnect.php
@@ -1,17 +1,19 @@
 <?php  
+
     class dbConnect {  
         function __construct() {  
-            require_once('config.php');  
-            $conn = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);  
-            mysql_select_db(DB_DATABSE, $conn);  
-            if(!$conn)// testing the connection  
-            {  
-                die ("Cannot connect to the database");  
-            }   
-            return $conn;  
+            require_once('config.php');
+            $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
+            if (!$conn) {
+                die("Cannot connect to the database: " . mysqli_connect_error());
+            }
+            echo "working";
+            return $conn;
+            
         }  
         public function Close(){  
             mysql_close();  
         }  
     }  
+
 ?>  
